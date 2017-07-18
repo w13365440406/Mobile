c13baaf0b69085938d11b5d6fa9749977edb2516
z1738736766 <1738736766@QQ.com>
2017-07-18 09:55:14 +0800

    张森

---
+++ js/Decoration_tow.js
@@ -0,0 +1,73 @@
+var text = [
+    {text_name:'<p>7月高温不下，同时也是秀腿的好时节，无论是大街小巷还是地铁卖场，基本都能遇到光腿美美。相信大家，看到白皙大长腿都会忍不住回看几眼，并由生羡慕，腿怎么可以这么白，这么长。这对腿短女生真的是很大的伤害，其实拉伸美腿，只需一招，只要提高腰线。</p>'},
+    {text_name:'<p><img src="http://img.fuwo.com/attachment/1707/17/34a39c4e6ada11e7b4fe00163e00254c.jpg" alt="" /></p>'},
+    {text_name:'<p>由于着装搭配问题，会给人带来不同的视觉冲击，但我们可以用腰线改变视觉。所以说腰线对服饰很重要，可以拉伸大长腿还能提升整个人气质，如果我们把腰线运用到<a href="http://www.fuwo.com/">家装</a>中，是不是也能让墙面扭出小蛮腰？</p>'},
+    {text_name:'<p><img src="http://img.fuwo.com/attachment/1707/17/34c893006ada11e7b4fe00163e00254c.jpg" alt="" /></p>'},
+    {text_name:'<p><b>一、贴腰线的墙面</b></p>'},
+    {text_name:'<p><b>1、 增加层次感，避免单一</b></p>'},
+    {text_name:'<p><img src="http://img.fuwo.com/attachment/1707/17/34ed89bc6ada11e7b4fe00163e00254c.jpg" alt="" /></p>'},
+    {text_name:'<p>一样色彩的墙面太单调，选择不一样的纹理在墙面加一条腰线，可以增加层次感还能划分区域，合理布局。</p>'},
+    {text_name:'<p><b>2、视觉冲击 美的享受</b></p>'},
+    {text_name:'<p><img src="http://img.fuwo.com/attachment/1707/17/3500e8866ada11e7b4fe00163e00254c.jpg" alt="" /></p>'},
+    {text_name:'<p> 根据不同空间布局采用不同腰线铺设，不仅在视觉上给人冲击，还能带来美的享受。</p>'},
+    {text_name:'<p><b>3、强化风格 风格更统一</b></p>'},
+    {text_name:'<p><img src="http://img.fuwo.com/attachment/1707/17/37a712226ada11e7b4fe00163e00254c.jpg" alt="" /></p>'},
+    {text_name:'<p>选择跟整体家装风格类似腰线搭配，风格更统一协调，还能点缀墙面。</p>'},
+    {text_name:'<p><b>二、不同空间采用不同腰线</b></p>'},
+    {text_name:'<p><img src="http://img.fuwo.com/attachment/1707/17/37d1f8986ada11e7b4fe00163e00254c.jpg" alt="" /></p>'},
+    {text_name:'<p>腰线将客厅墙面一分二，不再是堵赤裸裸白墙，还可以刷上两种不同颜色乳胶漆，腰线的高度与沙发持平，一切都是那么刚刚好，不会显得太突兀。同时也给人一种划分区域感觉，各有各的领地，各有各自布局，神圣不可侵犯。细腰线多用于客厅或卫浴。</p>'},
+    {text_name:'<p><img src="http://img.fuwo.com/attachment/1707/17/37df38fa6ada11e7b4fe00163e00254c.jpg" alt="" /></p>'},
+    {text_name:'<p>如果一条腰线觉得单一，那么四方腰线，将墙面划分成相框式，刚好可以用作照片墙，展示孩子们成长变化及生活美好点滴。这种腰线一般运用客厅背景墙。</p>'},
+    {text_name:'<p><img src="http://img.fuwo.com/attachment/1707/17/3da51d4a6ada11e7b4fe00163e00254c.jpg" alt="" /></p>'},
+    {text_name:'<p>有的家庭餐厅没有过多<a href="http://www.fuwo.com/">装饰</a>，只是简单的餐桌椅、窗帘，那么就可以利用墙面来一条细点腰线点缀。可以跟客厅腰线一致，细腰线显得格外苗条精致。</p>'},
+    {text_name:'<p><img src="http://img.fuwo.com/attachment/1707/17/3dc8c42a6ada11e7b4fe00163e00254c.jpg" alt="" /></p>'},
+    {text_name:'<p>这款厨房采用的宽腰线，选择与厨房瓷砖类似颜色，错开式铺设方法，凸显腰线美。</p>'},
+    {text_name:'<p><img src="http://img.fuwo.com/attachment/1707/17/3ddbb8006ada11e7b4fe00163e00254c.jpg" alt="" /></p>'},
+    {text_name:'<p>卫浴的双边腰线，上下对应铺设，整齐划一，双边腰线需要注意一点就是，腰线高度比例做好测量，不然会显得很压抑。</p>'},
+    {text_name:'<p><b>三、腰线高度设计一般有三种：</b></p>'},
+    {text_name:'<p>1、保守贴法：地面以上1米至1.1米</p>'},
+    {text_name:'<p>2、顶角线以下30厘米至40厘米</p>'},
+    {text_name:'<p>3、直接在顶角线下面拼贴，有视觉上提升高度的作用。</p>'},
+    {text_name:'<p><img src="http://img.fuwo.com/attachment/1707/17/3df1b93e6ada11e7b4fe00163e00254c.jpg" alt="" /></p>'},
+    {text_name:'<p>看了这么多墙面小蛮腰，是不是瞬间觉得家里太low了，很想来一打，不如来福窝硬装全包，包设计，包量房，设计专属你的家装方案。</p>'},
+    {text_name:'<p>本文有福窝网原创，作者幽兰，不得转载！</p>'},
+    {text_name:'<p><img src="http://img.fuwo.com/attachment/1707/17/3e010e026ada11e7b4fe00163e00254c.jpg" alt="" /></p>'},
+    {text_name:'<p>有关<b><a href="http://www.fuwo.com">装修</a>知识/<a href="http://www.fuwo.com/zhuangxiu/fengshui/">装修风水</a></b>攻略，请持续关注<b>装修攻略</b>，如果你对<b>福窝互联网装修套餐</b>还有想了解和咨询的，可点击底部免费获取报价，要好装修，就来福窝！</p>'},
+    ];
+// console.log(text);
+for (var i = 0 ; i < text.length ; i++) {
+    $('.theme_text').append(text[i].text_name);
+};
+var list =[
+    {
+        img:"http://img.fuwo.com/attachment/1608/19/4df08446659f11e6818900163e00254c.jpg@!200x200.jpg",
+        text:"腰线的存在驱散了墙面的单调沉闷，使室内元素变得丰富起来，在空间起到一种画龙点睛的作用同时，也反映了屋主主人的个性、品位和生活情趣。",
+        num:"82",
+        date:"2016年08月18日"
+    },
+    {
+        img:"http://img.fuwo.com/attachment/1604/21/16fe69d607b111e68e1500163e00254c.jpg@!200x200.jpg",
+        text:"儿童房墙纸贴装饰 腰线墙纸搭配设计儿童房里贴满色彩墙纸，小飘窗外加墨绿窗帘。",
+        num:"346",
+        date:"2016年04月21日"
+    },
+    {
+        img:"http://img.fuwo.com/attachment/1604/07/1bad008cfc6c11e589aa00163e00254c.jpg@!200x200.jpg",
+        text:"儿童房墙纸贴装饰图 腰线墙纸搭配设计儿童房里贴满色彩墙纸，小飘窗外加墨绿窗帘。",
+        num:"210",
+        date:"2016年04月07日"
+    },
+    {
+        img:"http://img.fuwo.com/attachment/1612/20/825271cec69b11e6901900163e00254c.jpg@!200x200.jpg",
+        text:"全球室内墙面漆十大品牌排名其实是一个经常在变化的排名，只要对此有过关注的业主应该就不会太陌生，毕竟经常有新的牌子出现，也有多老牌子被取代，因",
+        num:"0",
+        date:"2016年12月20日"
+    }
+];
+for (var i = 0 ; i < list.length ; i++) {
+    var div = $('<li><div><img src="'+list[i].img+'" alt=""><div><div><span>'+list[i].text+'</span></div><span><div>'+list[i].num+'</div><span>'+list[i].date+'</span></span></div></div></li>');
+    $(".theme_list>ul").append(div);
+};
+function afun () {
+    window.history.back();
+}
